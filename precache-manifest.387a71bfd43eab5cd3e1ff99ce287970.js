046805c0ddcf5b90e45dc9b5ba4df25ba29db123
neil <arifrahman@hotmail.com>
2019-11-04 16:35:04 -0500

    Updates

---
+++ precache-manifest.387a71bfd43eab5cd3e1ff99ce287970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d1646e7c8b088177fd3bd479bdc56a",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "111df35891af36389a88",
+    "url": "/clickygame/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2ada458591b8772f2eb9",
+    "url": "/clickygame/static/css/main.997c38a1.chunk.css"
+  },
+  {
+    "revision": "111df35891af36389a88",
+    "url": "/clickygame/static/js/2.d7a8826a.chunk.js"
+  },
+  {
+    "revision": "2ada458591b8772f2eb9",
+    "url": "/clickygame/static/js/main.cd962198.chunk.js"
+  },
+  {
+    "revision": "b4ed2c23f69c9b03baae",
+    "url": "/clickygame/static/js/runtime-main.f70fee65.js"
+  }
+]);
